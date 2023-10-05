96cc9af4be840281d0143166b2a0e8f1930a326d
eric <ericmitchell1625@hotmail.com>
2023-10-05 19:01:57 -0400

    Fibonacci.py solution

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,15 @@
 def fibonacci(n):
-  pass
+  sum = 0
+  num1 = 0
+  num2 = 1
+  count = 1
+
+  while (count < n):
+    sum = num1 + num2
+    num1 = num2
+    num2 = sum
+    count += 1
+  return sum
+
 
 
